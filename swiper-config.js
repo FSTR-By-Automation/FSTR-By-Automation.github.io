7c4a1b20038edc241f8a0900d9f0715bee5cb167
Andrew Ko <andy@cachevalley.tech>
2025-02-05 10:38:08 -0500

    Add files via upload

---
+++ swiper-config.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
